c8b1aa6a480a7ed9b94eb496f66b051ec38f9c24
evacosta <evacosta.if@gmail.com>
2025-09-09 08:54:47 -0300

    mini projeto

---
+++ python/calculadora_imc.py
@@ -0,0 +1,11 @@
+# Seu Primeiro Mini-Projeto - Calculadora de IMC
+
+# 1. Saudação inicial
+
+# 2. Pedir os dados para o usuário
+
+# 3. Calcular o IMC
+
+# 4. Mostrar o resultado
+
+# 5. Dar um feedback com base no resultado
