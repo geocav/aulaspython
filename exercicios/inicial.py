4803b4d6b1fab5e2452fabfcc41a28e5004c2707
Georgean Cavalcante <georgean.cavalcante@gmail.com>
2025-09-15 08:11:48 -0300

    resolução das tarefas

--- exercicios/inicial.py
+++ exercicios/inicial.py
@@ -5,32 +5,42 @@
 # Crie variáveis para armazenar as seguintes informações:
 
 # Seu nome (texto, string)
+nome = "georgean"
 
 # Sua idade (número inteiro, integer)
-
+idade = 28
 # Sua cidade (texto, string)
-
+cidade = "teresina"
 # Exemplo de saída: Olá, meu nome é João, tenho 25 anos e moro em Salvador.
-
+print("Olá, meu nome é", nome, ", tenho", idade, "anos e moro em", cidade)
 # Exercício 2: Calculadora Simples
 # Crie duas variáveis, numero1 e numero2, e atribua um valor numérico a cada uma.
-
+num1 = 10
+num2 = 21
 # Crie variáveis para armazenar os resultados das quatro operações matemáticas básicas: soma, subtracao, multiplicacao e divisao.
-
+x = num1+num2
+y = num1-num2
+z = num1*num2
+w = num1/num2
 
 # Exemplo de saída:
 
 # A soma de 10 e 5 é: 15
 # A multiplicação de 10 e 5 é: 50
+print("a soma de 10 + 21 = ", x)
+print("a subtração de 10 + 21 = ", y)
+print("a multiplicação de 10 + 21 = ", z)
+print("a divisão de 10 + 11 = ", w)
+
 # Nível 2: Tornando Interativo (input())
 # Agora, vamos criar programas que conversam com o usuário, pedindo informações.
-
 # Exercício 3: Eco Personalizado
 # Pergunte o nome do usuário.
-
+nome2 = input("qual teu nome? ")
 # Pergunte qual a sua comida favorita.
-
+comida = input("qual tua comida preferida?? ")
 # Exiba uma mensagem personalizada usando as respostas.
+print ("Olá,", nome2,"Eu vejo que sua comida favorita é", comida, ". Que delícia!")
 
 # Exemplo de interação:
 
@@ -42,11 +52,10 @@
 
 
 # Calcule a idade do usuário em dias (considere que um ano tem 365 dias).
-
+idade2 = int(input("qual tua idade: ")) 
 # Exiba o resultado.
-
+print("vc já viveu", idade2*365, "dia")
 # Exemplo de interação:
-
 # Quantos anos completos você tem? 30
 # Você já viveu aproximadamente 10950 dias!
 
@@ -55,12 +64,18 @@
 
 # Exercício 5: Verificador de Aprovação
 # Peça para o usuário digitar a nota de um aluno (de 0 a 10).
-
+nota = float(input("digite sua nota de 0 a 10"))
 # Converta a nota para um número (pode ser float() para aceitar notas como 7.5).
 
 # Crie uma estrutura condicional:
 
 # Se a nota for maior ou igual a 7.0, exiba "Aluno Aprovado!".
+if nota >= 7:   
+    print ("aluno aprovado")
+elif nota < 7 and nota >= 5:
+    print("Aluno em Recuperação")  
+else:
+    print("Aluno Reprovado")
 
 # Se a nota for menor que 7.0 mas maior ou igual a 5.0, exiba "Aluno em Recuperação.".
 
@@ -68,9 +83,12 @@
 
 # Exercício 6: Par ou Ímpar?
 # Peça ao usuário para digitar um número inteiro.
-
+numimpar = int(input("digite um numero"))
 # Use o operador de módulo % para descobrir se o número é par ou ímpar. O operador % retorna o resto de uma divisão. 
-
+if numimpar%2 == 0:
+    print ("numero par")
+else:
+    print ("numero impar")
 # Exiba uma mensagem informando se o número é par ou ímpar.
 
 # Exemplo de interação:
@@ -107,3 +125,14 @@
 # Desconto: R$ 30.0
 # Preço Final: R$ 120.0
 # Parabéns! Sua compra tem frete grátis!
+
+valorTotal = float(input("preço da compra"))
+valorDesconto = float(input("valor do desconto"))
+precoFinal = valorTotal - (valorTotal * valorDesconto / 100)
+
+print("Preço Original: ", valorTotal)
+print("Descont: ", valorDesconto)
+print("Preço Final: ", precoFinal)
+
+if precoFinal >= 100 :
+    print ("Parabéns! Sua compra tem frete grátis!")
