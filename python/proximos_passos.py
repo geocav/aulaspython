1ded3c2b8b9d709da084f37f15d3d040dfad31f9
evacosta <evacosta.if@gmail.com>
2025-09-15 09:07:08 -0300

    conteudo previsto proxima aula

--- python/proximos_passos.py
+++ python/proximos_passos.py
@@ -1,3 +1,3 @@
-# Listas e Dicionários: Maneiras de guardar múltiplos dados em uma única variável.
-# Laços de Repetição (for e while): Para executar uma mesma tarefa várias vezes sem repetir código.
+# Dicionários: Maneiras de guardar múltiplos dados em uma única variável.
+# Laços de Repetição ( while): Para executar uma mesma tarefa várias vezes sem repetir código.
 # Funções: Para organizar seu código em blocos reutilizáveis.
