3f71d9e8805f0fad9eb601a99edf59423e45b2e4
evacosta <evacosta.if@gmail.com>
2025-09-22 13:07:15 -0300

    passos

--- python/proximos_passos.py
+++ python/proximos_passos.py
@@ -1,7 +1,7 @@
-Persistência de Dados: Leitura e Escrita de Arquivos
-Lidando com o Inesperado: Tratamento de Erros
-Módulos e Bibliotecas Externas
+# Persistência de Dados: Leitura e Escrita de Arquivos
+# Lidando com o Inesperado: Tratamento de Erros
+# Módulos e Bibliotecas Externas
 
-Organizando Projetos Maiores
-Criação de Módulos
-Ambientes Virtuais
+# Organizando Projetos Maiores
+# Criação de Módulos
+# Ambientes Virtuais
