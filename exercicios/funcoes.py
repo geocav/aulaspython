c70548a2f9cfbf73c9f30a17daea458ed6db40bc
evacosta <evacosta.if@gmail.com>
2025-09-29 16:09:39 -0300

    EXERCICIOS

--- exercicios/funcoes.py
+++ exercicios/funcoes.py
@@ -1,25 +1,32 @@
 # Exercício 1: Catálogo de Produtos (Dicionários)
 
-# 1.  Crie um dicionário para representar um produto de um e-commerce. Ele deve conter as seguintes chaves: `codigo`, `nome`, `categoria`, `preco` e `em_estoque` (este último com um valor booleano `True` ou `False`).
+# 1.  Crie um dicionário para representar um produto de um e-commerce. 
+# Ele deve conter as seguintes chaves: `codigo`, `nome`, `categoria`, `preco` e `em_estoque` 
+# (este último com um valor booleano `True` ou `False`).
 # 2.  Imprima o nome e o preço do produto, acessando os valores através de suas chaves.
 # 3.  Simule uma promoção: altere o valor da chave `preco`, aplicando um desconto de 10%.
 # 4.  Verifique o valor da chave `em_estoque`. Se for `True`, imprima "Produto disponível para compra\!". Caso contrário, imprima "Produto esgotado.".
-# produto = {
-#     "codigo": "12345",
-#     "nome": "camisa",
-#     "categoria": "roupa", 
-#     "preco": 100,
-#     "em_estoque": False
-#     }
-# print(produto["preco"])
-# print(produto["nome"])
-# produto["preco"] = produto["preco"] * 0.9
-# print(produto["preco"])
+produto = {
+    "codigo": "12345",
+    "nome": "camisa",
+    "categoria": "roupa", 
+    "preco": 100,
+    "em_estoque": False
+    }
+print(produto["preco"])
+print(produto["nome"])
+produto["preco"] = produto["preco"] * 0.9
+# É a forma mais curta e comum para aplicar operação na propria variavel
+# produto["preco"] *= 0.9
+print(produto["preco"])
 
-# if (produto["em_estoque"] == True ):
-#     print("Produto disponível para compra\!")
-# else:    
-#     print("Produto esgotado.")
+# Como o valor esperado já é True ou false, elimina a redundancia.
+# if (produto["em_estoque"]):
+if (produto["em_estoque"] == True ):
+    print("Produto disponível para compra\!")
+else:    
+    print("Produto esgotado.")
+
 # Exercício 2: Jogo de Adivinhação (Laço `while`)
 
 # 1.  Defina um número secreto (por exemplo, `numero_secreto = 42`).
@@ -27,24 +34,35 @@
 # 3.  Dentro do laço, peça para o usuário digitar um palpite. Não se esqueça de converter o `input` para `int`.
 # 4.  Adicione uma condição `if` dentro do laço: se o usuário acertar, exiba uma mensagem de parabéns e use o comando `break` para sair do laço.
 # 5.  Se o usuário errar, o laço continuará naturalmente.
-# numero_secreto = 42
 
-# while numero_secreto :
-#     chute = int(input("qual o numero que estou pensando ??"))
-#     if chute == numero_secreto:
-#      print("ACERTOU")
-#      break
+numero_secreto = 42
+
+while numero_secreto :
+    chute = int(input("qual o numero que estou pensando ??"))
+    if chute == numero_secreto:
+     print("ACERTOU")
+     break
+
+#while True, cria um laço infinito
+while True :
+    chute = int(input("qual o numero que estou pensando ??"))
+    if chute == numero_secreto:
+        print("ACERTOU")
+        break
+    else:
+        print("Errado, tente novamente!!")
 # Exercício 3: Conversor de Temperatura (Funções)
 
 # 1.  Crie uma função chamada `celsius_para_fahrenheit` que recebe um parâmetro: `temperatura_celsius`.
 # 2.  Dentro da função, aplique a fórmula de conversão: `F = (C * 9/5) + 32`.
 # 3.  A função deve usar `return` para devolver o resultado em Fahrenheit.
 # 4.  Fora da função, chame-a com um valor (ex: `30` graus Celsius) e imprima o resultado de forma clara.
-# def celsius_para_fahrenheit (temperatura_celsius):
-#     F = (temperatura_celsius * 9/5) + 32
-#     print(F)
-#     return
-# celsius_para_fahrenheit (30)
+
+#Exerça uma atividade, sabendo só o necessário para ela funcionar e faça uma unica coisa
+def celsius_para_fahrenheit (temperatura_celsius):
+    return (temperatura_celsius * 9/5) + 32
+
+print(celsius_para_fahrenheit(30))
 
 # Exercício 4: Validação de Login (Função com Dicionário)
 
@@ -56,31 +74,40 @@
 #       * A função deve retornar `True` se o login for válido, e `False` caso contrário.
 # 4.  Peça para o usuário digitar seu nome de usuário e senha, e use sua função para imprimir se o acesso foi "Permitido" ou "Negado".
 # 1. Criando o dicionário de usuários
-# usuarios = {
-#     "admin": "1234",
-#     "joao": "senha123"
-# }
+usuarios = {
+    "admin": "1234",
+    "joao": "senha123"
+}
 
+def validar_login(usuario, senha):
+    if usuario in usuarios:
+        if usuarios[usuario] == senha:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
 # def validar_login(usuario, senha):
-#     if usuario in usuarios:
-#         if usuarios[usuario] == senha:
-#             return True
-#         else:
-#             return False
-#     else:
-#         return False
-
-# while usuarios:
-#     login = input("Digite seu login: ")
-#     senha = input("Digite sua senha: ")
+    # verdadeiro (e/and) verdadeiro = verdadeiro
+    # verdadeiro (e/and) false = falso
+    # verdadeiro (ou/or) verdadeiro = verdadeiro
+    # verdadeiro (ou/or) false = verdadeiro
+    # false (ou/or) false = false
+    # return usuario in usuarios and usuarios[usuario] == senha
 
 
-#     if validar_login(login, senha) == True:
-#         print("Acesso Permitido")
-#         break
-#     else:
-#         print("Acesso Negado")
-    #     print("TENTE NOVAMENTE")  
+while usuarios:
+    login = input("Digite seu login: ")
+    senha = input("Digite sua senha: ")
+
+
+    if validar_login(login, senha) == True:
+        print("Acesso Permitido")
+        break
+    else:
+        print("Acesso Negado")
+        print("TENTE NOVAMENTE")  
 
 
 # Exercício 5: Gerador de Contagem (Função com `while`)
@@ -99,8 +126,16 @@
 #             print(inicio)
 #             inicio = inicio-1
 #     print("FOGO")
-# numero = int(input("digite a contagem"))
-# contagem_regressiva(numero)
+
+
+def contagem_regressiva(inicio):
+    while inicio > 0:
+        print(inicio)
+        inicio -= 1 #ou inicio = inicio-1
+
+    print("FOGO")
+numero = int(input("digite a contagem"))
+contagem_regressiva(numero)
 
 
 
@@ -135,13 +170,23 @@
 #     Se a escolha for '3', imprima "Até logo\!" e use `break` para encerrar o laço `while`.
 #     Se a escolha for inválida, mostre uma mensagem de erro.
 agenda = {}
-def adicionar_contato(nome, telefone):
+
+agenda = {'eva': '1234'}
+
+# def adicionar_contato(, nome, telefone): dependencia oculta
+# def adicionar_contato(agenda, nome, telefone): contrato fica mais claro
+def adicionar_contato(agenda, nome, telefone):
     agenda[nome] = telefone
-    print("DEU CERTO")
+    print(f"Contato '{nome}' adicionado com sucesso!")
 
 def ver_contatos(agenda):
-    for contato in agenda:
-        print(agenda.items()) 
+    print("\n--- Sua Agenda de Contatos ---")
+    if not agenda:
+        print("A agenda está vazia.")
+        return
+
+    for nome, telefone in agenda.items():
+        print(f"Nome: {nome} | Telefone: {telefone}")
 
 while True:
     print("1-Adicionar Contato")
@@ -151,8 +196,79 @@ while True:
     if numero == "1":
         nom = input("Qual seu nome??")
         tel = input("qual seu telefone??")
-        adicionar_contato(nom,tel)
-    if numero == "2":
+        adicionar_contato(agenda, nom,tel)
+    elif numero == "2":
         ver_contatos(agenda)
-    if numero == "3":
+    elif numero == "3":
         break
+    else:
+        print("Opção invalida")
+
+
+
+
+# ========================================== A FAZER
+
+# Jogo de Adivinhação (Laço `while`) -   Variação
+
+# Informe ao usuario se o valor que ele forneceu é maior ou menor ao valor esperado.
+numero_secreto = 42
+
+while True :
+    chute = int(input("qual o numero que estou pensando ??"))
+    if chute == numero_secreto:
+     print("ACERTOU")
+     break
+
+# ======================================================
+# Nível Intermediário: Caixa Registradora de Lanchonete
+# Objetivo: Criar um sistema de linha de comando que permita a um atendente registrar os itens de um pedido e calcular o total da conta.
+
+# Requisitos:
+
+# O programa deve ter um cardápio predefinido com nomes de produtos e seus respectivos preços.
+
+# O sistema deve permitir que o usuário adicione múltiplos itens ao pedido, um de cada vez.
+
+# O programa deve validar se o item inserido pelo usuário existe no cardápio. Itens inválidos não devem ser adicionados ao pedido.
+
+# Deve haver uma forma de o usuário sinalizar que terminou de adicionar itens.
+
+# Ao final, o programa deve exibir um resumo de todos os itens do pedido e o valor total a ser pago.
+
+# ======================================================
+
+# Nível Avançado: Sistema de Votação Simples
+# Objetivo: Desenvolver um programa que simule uma urna eletrônica, registrando votos para candidatos predefinidos e apurando o resultado final.
+
+# Requisitos:
+
+# O programa deve iniciar com uma lista de candidatos, todos com zero votos.
+
+# O sistema deve permitir que múltiplos votos sejam registrados sequencialmente.
+
+# O programa deve rejeitar votos para candidatos que não existam na lista oficial.
+
+# O usuário deve ter uma maneira de encerrar o processo de votação.
+
+# Após o encerramento, o programa deve exibir a contagem de votos de cada candidato e declarar 
+# quem foi o vencedor (o candidato com o maior número de votos).
+
+# =============================================================
+
+# Nível Desafio: Mini Jogo RPG - Batalha em Turnos
+# Objetivo: Construir um jogo de batalha baseado em turnos, onde um herói controlado pelo jogador luta contra um monstro controlado pelo computador até que um deles seja derrotado.
+
+# Requisitos:
+
+# O herói e o monstro devem ser representados por estruturas de dados que contenham seus atributos: no mínimo, nome, vida, ataque e defesa.
+
+# A batalha deve ocorrer em turnos. Em cada rodada, o herói ataca primeiro, seguido pelo ataque do monstro (se ele ainda estiver vivo).
+
+# O dano de um ataque deve ser calculado com base nos atributos de ataque do atacante e de defesa do defensor. A lógica para este cálculo fica a seu critério.
+
+# A cada turno, o status atual (pelo menos a vida) de ambos os personagens deve ser exibido ao jogador.
+
+# O jogo termina quando a vida de um dos personagens chegar a 0 ou menos.
+
+# Ao final da batalha, uma mensagem declarando o vencedor (Herói ou Monstro) deve ser exibida.
